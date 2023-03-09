aaac229a4a0179209453197d2ca08fd87dae37f2
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-03-09 02:28:30 +0200

    WIP

---
+++ app/Http/Controllers/ProjectInvitationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProjectInvitationRequest;
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProjectInvitationController extends Controller
+{
+    public function store(Project $project, ProjectInvitationRequest $request){
+
+        $this->authorize('update',$project);
+
+
+        $user = User::whereEmail(request('email'))->first();
+
+        $project->invite($user);
+
+        return redirect($project->path());
+
+    }
+}
